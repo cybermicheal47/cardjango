d677820cd92c6776eb567953d6693bd620f51887
micheal lyon <cybermicheal47@gmail.com>
2021-03-16 22:28:39 +0100

    complete team model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone',
+        'USER': 'postgres',
+        'PASSWORD': '55322012',
+        'HOST': 'localhost',
     }
 }
 
@@ -124,3 +127,9 @@ STATICFILES_DIRS = [
 
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
+
+
+#MEDIA SETTINGS
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
